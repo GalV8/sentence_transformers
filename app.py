d40c27546a5d9d4e48ec0cb41a262c59105f2482
GalV8 <57728772+GalV8@users.noreply.github.com>
2024-07-18 12:33:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,16 @@ def index():
 @app.route('/embed', methods=['POST'])
 def embed_text():
     try:
-        # Get JSON data from request
         data = request.get_json()
-        department_sentences_dict = data.get('sentences')
-        if not department_sentences_dict:
+
+        if not data or 'sentences' not in data:
             return jsonify({"error": "No sentences provided"}), 400
 
-        # Check if the input is a dictionary as expected
+        department_sentences_dict = data['sentences']
+
         if not isinstance(department_sentences_dict, dict):
             return jsonify({"error": "Sentences must be provided as a dictionary with product IDs as keys"}), 400
 
-        # Prepare for embedding
         embedded_results = {}
         for product_id, department_sentences in department_sentences_dict.items():
             if not isinstance(department_sentences, dict):
@@ -29,9 +28,7 @@ def embed_text():
 
             embedded_sentences = {}
             for dept_code, sentence in department_sentences.items():
-                # Generate embeddings for each sentence, assuming each entry is a single string
                 embedding = model.encode(sentence, show_progress_bar=False)
-                # Convert embedding to list for JSON serialization
                 embedded_sentences[dept_code] = embedding.tolist()
 
             embedded_results[product_id] = embedded_sentences
